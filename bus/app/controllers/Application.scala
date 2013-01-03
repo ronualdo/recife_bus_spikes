c0638ee6d601a5346f699afb17cf16773816923a
Leandro Gualter <leandrogualter@gmail.com>
2013-01-03 16:15:55 -0300

    creates a json service to provide routes and stops

--- bus/app/controllers/Application.scala
+++ bus/app/controllers/Application.scala
@@ -9,10 +9,30 @@ import anorm.SqlParser._
 import recifeBuses._
 import models._
 import play.Logger
+import play.api.libs.json.Json._
 
 object Application extends Controller {
   def index = Action {
     Ok(views.html.index(Route.find(externalRouteId = "744")))
+  }
+  
+  def stopsFor(routeId: String) = Action {
+    val routes = Route.find(externalRouteId= routeId).map { route =>
+      Map("name" -> route.name,
+        "externalRouteId" -> route.externalRouteId,
+        "nomeItinerario" -> route.nomeItinerario,
+        "stops" -> toJson(route.stops.map {s =>
+            Map("codigo" -> s.codigo,
+              "bairro" -> s.bairro,
+              "logradouro" -> s.logradouro,
+              "referencia" -> s.referencia,
+              "latitude" -> s.latitude,
+              "longitude" -> s.longitude)
+          }).toString
+       )
+    }
+    
+    Ok(toJson(routes).toString)
   }
 
   def reload = Action {
